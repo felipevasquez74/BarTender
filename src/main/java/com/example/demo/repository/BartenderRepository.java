4145934db98aa17f74dc1f7017e1412fc379c3c7
afvasquez <felipevasqueortiz@gmail.com>
2022-10-23 20:59:34 -0500

    se agrega notacion de repository

--- src/main/java/com/example/demo/repository/BartenderRepository.java
+++ src/main/java/com/example/demo/repository/BartenderRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.ArraysBartender;
 
+@Repository
 public interface BartenderRepository extends JpaRepository<ArraysBartender, Integer> {
 
 }
